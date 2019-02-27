8adc5485ca98236af3834ba93ab549a3d9d786ad
Mehmed Adzemovic <mehmed.adzemovic@gmail.com>
2019-02-27 19:32:22 +0100

    Enkelt forarbeid
    La til registerstruct og deklarasjonar

--- 4_accelerometer/accel.h
+++ 4_accelerometer/accel.h
@@ -1,6 +1,9 @@
 #ifndef ACCEL_H
 #define ACCEL_H
 
+
+//Pin 27 og 28 er ACC INT1 og ACC INT2 (akselerometer)
+//Pin 0 og 30 er SCL og SDA
 void accel_init();
 
 void accel_read_x_y_z(int * data_buffer);
---
+++ 4_accelerometer/twi.h
@@ -0,0 +1,59 @@
+#ifndef TWI_H
+#define TWI_H
+#include <stdint.h>
+
+#define TWIO ((NRF_TWI_REG*)0x40003000)
+
+typedef struct {
+	//Tasks
+	volatile uint32_t STARTRX;
+	volatile uint32_t RESERVED0;
+	volatile uint32_t STARTTX;
+	volatile uint32_t RESERVED1[2];
+	volatile uint32_t STOP;
+	volatile uint32_t RESERVED2;
+	volatile uint32_t SUSPEND;
+	volatile uint32_t RESUME;
+	volatile uint32_t RESERVED3[56];
+	//Events
+	volatile uint32_t STOPPED;
+	volatile uint32_t RXDREADY;
+	volatile uint32_t RESERVED4[4];
+	volatile uint32_t TXDSENT;
+	volatile uint32_t RESERVED5;
+	volatile uint32_t ERROR;
+	volatile uint32_t RESERVED6[4];
+	volatile uint32_t BB;
+	volatile uint32_t RESERVED7[49];
+	//Registers
+	volatile uint32_t SHORTS;
+	volatile uint32_t RESERVED8[63];
+	volatile uint32_t INTEN;
+	volatile uint32_t INTENSET;
+	volatile uint32_t INTENCLR;
+	volatile uint32_t RESERVED9[110];
+	volatile uint32_t ERRORSRC;
+	volatile uint32_t RESERVEDA[14];
+	volatile uint32_t ENABLE;
+	volatile uint32_t RESERVEDB;
+	volatile uint32_t PSELSCL;
+	volatile uint32_t PSELSDA;
+	volatile uint32_t RESERVEDC[2];
+	volatile uint32_t RXD;
+	volatile uint32_t TXD;
+	volatile uint32_t RESERVEDD;
+	volatile uint32_t FREQUENCY;
+	volatile uint32_t RESERVEDE[24];
+	volatile uint32_t ADDRESS;
+} NRF_TWI_REG;
+
+void twi_init();
+
+void twi_multi_read(
+	uint8_t slave_address,
+	uint8_t start_register,
+	int registers_to_read,
+	uint8_t *data_buffer
+);
+
+#endif
